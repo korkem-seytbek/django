244e2da720e403ec2c713c8818bc32f2e41cd69a
korkem-seytbek <korkem.erbolkyzy@icloud.com>
2025-04-08 17:26:49 +0500

    Initial Django project

--- cw_5/posts/urls.py
+++ cw_5/posts/urls.py
@@ -4,8 +4,10 @@ from .views import *
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
-    path('posts/', all_posts, name='all_posts'),
-    path('posts/my', my_posts, name='my_posts'),
-    path('posts/<int:id>/', post_detail, name='post_detail'),
-    path('posts/<int:id>/delete', delete_post, name='delete_post'),
+    path('posts/', posts_view, name='posts'),
+    path('posts/my', my_posts_view, name='my_posts'),
+    path('posts/<int:id>/', post_detail_view, name='post_detail'),
+    path('posts/create/', create_post_view, name='create_post'),
+    path('posts/<int:id>/delete/', delete_post_view, name='delete_post'),
 ]
+
